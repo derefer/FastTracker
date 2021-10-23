4d390e8db5ad9d08f47e2e2c481745b5382589e2
Ferenc Kovacs <derefer@gmail.com>
2021-10-23 20:45:59 +0200

    Add some basic functionality.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 A basic intermittent fasting app for Android.
+
+User stories:
+1) The user wants to track if he's fasting or not fasting. -> Done
+2) The user wants to track since when he's fasting or not fasting.
--- app/src/main/java/com/example/fasttracker/MainActivity.java
+++ app/src/main/java/com/example/fasttracker/MainActivity.java
@@ -2,13 +2,63 @@ package com.example.fasttracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private Button fastingToggleButton;
+    private TextView fastingStatusTextView;
+
+    private boolean isFasting;
+    private SharedPreferences sharedPreferences;
+    private static final String LOG_TAG = MainActivity.class.getName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        loadIsFasting();
+        initUi();
+    }
+
+    private void loadIsFasting() {
+        sharedPreferences = getPreferences(Context.MODE_PRIVATE);
+        isFasting = sharedPreferences.getBoolean(getString(R.string.is_fasting), false);
+    }
+
+    private void initUi() {
+        fastingStatusTextView = findViewById(R.id.fastingStatusTextView);
+        fastingToggleButton = findViewById(R.id.fastingToggleButton);
+        if (isFasting) {
+            fastingStatusTextView.setText(R.string.you_re_fasting);
+            fastingToggleButton.setText(R.string.stop_fasting);
+        } else {
+            fastingStatusTextView.setText(R.string.you_re_not_fasting);
+            fastingToggleButton.setText(R.string.start_fasting);
+        }
+        fastingToggleButton.setOnClickListener(v -> {
+            Log.i(LOG_TAG, "onClick()");
+            if (isFasting) {
+                isFasting = false;
+                fastingStatusTextView.setText(R.string.you_re_not_fasting);
+                fastingToggleButton.setText(R.string.start_fasting);
+            } else {
+                isFasting = true;
+                fastingStatusTextView.setText(R.string.you_re_fasting);
+                fastingToggleButton.setText(R.string.stop_fasting);
+            }
+        });
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putBoolean(getString(R.string.is_fasting), isFasting);
+        editor.apply();
     }
 }
